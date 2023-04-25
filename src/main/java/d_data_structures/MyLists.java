b9ae694c42f00fb898eb81f26feb736d4967ecda
AdrianBuchta <90181742+AdrianBuchta@users.noreply.github.com>
2023-04-25 20:16:06 +0200

    Update MyLists.java
    Data structures - Exercise 3

--- src/main/java/d_data_structures/MyLists.java
+++ src/main/java/d_data_structures/MyLists.java
@@ -1,4 +1,33 @@
 package d_data_structures;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class MyLists {
+
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Han Solo");
+        names.add("Luke Skywalker");
+        names.add("Darth Vader");
+        names.add("C-3PO");
+        names.add("Leia Organa");
+        names.add("Obi Wan Kenobi");
+
+        System.out.println("Names list:" + names);
+        System.out.println("Last element of the list: " + names.get(names.size() - 1));
+
+        ArrayList<String> temp = new ArrayList<>();
+        for(int i = names.size()-1; i >= 0; i--){
+            temp.add(names.get(i));
+        }
+
+        System.out.println("List in reverse order: " + temp);
+
+        names.sort(Comparator.naturalOrder());
+        System.out.println("Sorted ArrayList: " + names);
+
+        System.out.println("First element of the list: " + names.get(0));
+    }
+
 }
